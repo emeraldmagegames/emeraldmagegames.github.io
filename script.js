2dc97a6e649f30202ca5289883c70e39a8738ed0
emeraldmagegames <emeraldmagegames@proton.me>
2024-10-07 16:54:48 +1100

    Update script.js

--- script.js
+++ script.js
@@ -109,7 +109,7 @@ window.onload = function() {
             const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
 
             // When the stars are within 75px, pull them directly to the cursor/touch
-            if (distance < 75) {
+            if (distance < 100) {
                 // Move the star directly to the cursor's position
                 star.style.transform = `translate(${deltaX}px, ${deltaY}px)`;
             } else {
